f4bb26e8cbc86ff8a40c6ebd74d3da3440493791
sonnonet <korea.sonnonet@gmail.com>
2021-12-05 15:04:33 +0900

    Update testDHT.py

--- testDHT.py
+++ testDHT.py
@@ -2,17 +2,16 @@ import serial
 
 port = '/dev/ttyACM0'
 brate = 9600
-cmd = 'temp'
 
 seri = serial.Serial(port, baudrate = brate, timeout = None)
 print(seri.name)
 
-seri.writelines(cmd)
+seri.write('b\x0101')
 
 a = 1
 while a:
     if seri.in_waiting !=0 :
         content = seri.readline()
-        print(content)
+        print(content.decode())
         a=0
 
